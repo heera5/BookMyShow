2d054b5ac9ef6c0babd64d5f057f208728b0ce04
unknown <heera7358@gmail.com>
2024-02-12 11:01:26 +0530

    updated

--- src/main/java/com/example/bootproject/BookMyShow/repo/TheatreAdminRepo.java
+++ src/main/java/com/example/bootproject/BookMyShow/repo/TheatreAdminRepo.java
@@ -2,8 +2,9 @@ package com.example.bootproject.BookMyShow.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.bootproject.BookMyShow.entity.Theatre;
 
-public interface TheatreAdminRepo extends JpaRepository<Theatre, Integer> {
+import com.example.bootproject.BookMyShow.entity.TheatreAdmin;
+
+public interface TheatreAdminRepo extends JpaRepository<TheatreAdmin, Integer> {
 
 }
